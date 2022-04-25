99cc6487b606c751e53989188bb82fd22d02dcf5
Vasco <vascbarreiros@gmail.com>
2022-04-25 10:30:52 +0200

    fichier css reset / globalStyles ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+    return (
+        <>
+            <GlobalStyles />
+            <main className="App">
+                App File
+            </main>
+        </>
+    );
+}
+
+export default App;
